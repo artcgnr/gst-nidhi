24dace46ce080d434309739fc9cddd2cdf591cf5
artcgnr <artcgnrnidhi@gmail.com>
2026-08-18 11:25:20 +0530

    main

--- db.js
+++ db.js
@@ -2,13 +2,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.1/fireba
 import { getFirestore, collection, addDoc, getDoc, getDocs, query, where, orderBy, doc, setDoc, updateDoc, deleteDoc } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBA0eSQposFN_YhB4F3K2Q4uUzwSPcnZCc",
-    authDomain: "mytest-db-615d2.firebaseapp.com",
-    databaseURL: "https://mytest-db-615d2-default-rtdb.firebaseio.com",
-    projectId: "mytest-db-615d2",
-    storageBucket: "mytest-db-615d2.firebasestorage.app",
-    messagingSenderId: "7667456156",
-    appId: "1:7667456156:web:9daa4c020a486db62fb559"
+    apiKey: "AIzaSyBccHBwl_wcr80w96lkiUyeieBzmd2uKGs",
+    authDomain: "nidhi-gst.firebaseapp.com",
+    projectId: "nidhi-gst",
+    storageBucket: "nidhi-gst.firebasestorage.app",
+    messagingSenderId: "503817715778",
+    appId: "1:503817715778:web:395213b4faab86d56d2ddb"
 };
 
 const app = initializeApp(firebaseConfig);
